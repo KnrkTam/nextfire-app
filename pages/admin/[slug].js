36b2465f782ed854fd52563f1a1f3ed8936798ba
kenrick tam <kenricktam@gmail.com>
2022-07-24 08:40:27 -0600

    configured form validation

--- pages/admin/[slug].js
+++ pages/admin/[slug].js
@@ -53,8 +53,12 @@ function PostManager() {
 }
 
 function PostForm({ defaultValues, postRef, preview }) {
-  const { register, handleSubmit, reset, watch } = useForm({ defaultValues, mode: 'onChange' });
+  const { register, handleSubmit, reset, watch, formState, formState: { errors } } = useForm({ defaultValues, mode: 'onChange' });
    
+  const { isValid, isDirty } = formState; 
+
+
+
   const updatePost = async ({ content, published }) => {
     await postRef.update({
       content,
@@ -75,14 +79,20 @@ function PostForm({ defaultValues, postRef, preview }) {
       )}
 
       <div className={preview? styles.hidden : styles.controls}>
-        <textarea {...register('content')}></textarea>
+        <textarea {...register('content', {
+          maxLength: {value: 20000, message: 'content is too long'},
+          minLength: {value: 10, message: 'content is too short'},
+          required: {value: true, message: 'content is required'}
+        })}></textarea>
 
+        {errors.content && <p className="text-danger">{errors.content.message}</p>}
+        
         <fieldset>
           <input className={styles.checkbox} type="checkbox" {...register('published')} />
           <label>Published</label>
         </fieldset>
 
-        <button type="submit" className="btn-green">
+        <button type="submit" className="btn-green" disabled={!isDirty || !isValid }>
           Save Changes
         </button>
       </div>
