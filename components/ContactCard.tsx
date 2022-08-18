2e7792df574c8ab167d2662053da5b8bf12c7994
kenrick tam <kenricktam@gmail.com>
2022-08-18 11:31:48 -0400

    new conatct card button layout & new resume

--- components/ContactCard.tsx
+++ components/ContactCard.tsx
@@ -9,7 +9,7 @@ export default function ContactCard({}) {
 
     return (
         <>
-        <div className="card card-info lg:w-1/4 shadow-xl flex items-center flex-col" > 
+        <div className="card card-info lg:w-1/4 shadow-xl flex items-center flex-col lg:p-5 lg:pt-10" > 
             <img className="rounded-full mx-auto profile-img" src="profile_pic.jpg" alt="my profile photo"/>
             <h2 className='font-extrabold mt-2 text-white'>Kenrick Tam</h2>
             <h3 className="">Web developer</h3>
@@ -19,30 +19,30 @@ export default function ContactCard({}) {
                 <p>📧 kenricktam@gmail.com</p>
                 <p>📱 (437) 973 - 8229</p>
             </div>
-            <span className="flex gap-2 mb-2">
+            <span className="flex gap-2 mb-2 rounded-3xl bg-blue-800 p-3 w-2/5 place-content-around lg:w-full">
                 <Link href="https://github.com/knrktam">
                     <a target="_blank">
-                        <GitHubIcon className="w-8 h-8 transition transform cursor-pointer text-blueGray-400 hover:text-gray-600 hover:bg-white-600 hover:scale-110 duration-600" />
+                        <GitHubIcon className="w-8 h-8 transition-all transform cursor-pointer text-blueGray-400 hover:text-gray-900 hover:bg-white-600 hover:scale-125 duration-600" />
                     </a>
                 </Link>  
                 <Link href="https://www.linkedin.com/in/kenrick-tam-39625915b/">
                     <a target="_blank">
-                        <LinkedInIcon className="w-8 h-8 transition transform cursor-pointer text-blueGray-400 hover:text-blue-600 hover:scale-110 duration-600" />
+                        <LinkedInIcon className="w-8 h-8 transition transform cursor-pointer text-blueGray-400 hover:text-blue-600 hover:scale-125 duration-600" />
                     </a>
                 </Link>
             
                 <a href="" onClick={contactMe}>
-                    <EmailIcon className="w-8 h-8 transition transform cursor-pointer text-blueGray-400 hover:text-orange-600 hover:scale-110 duration-600" />
+                    <EmailIcon className="w-8 h-8 transition transform cursor-pointer text-blueGray-400 hover:text-orange-600 hover:scale-125 duration-600" />
 
                 </a>
                 <Link href="https://wa.me/14379748229?text=Hello World!">
                   <a target="_blank">
-                    <WhatsAppIcon className="w-8 h-8 transition transform cursor-pointer text-blueGray-400 hover:text-green-600 hover:scale-110 duration-600" />
+                    <WhatsAppIcon className="w-8 h-8 transition-all transform cursor-pointer text-blueGray-400 hover:text-green-600 hover:scale-125 duration-600" />
                   </a>
                 </Link>
             </span>
             <div className='flex flex-col items-center lg:items-start'>
-                <a href="/resume.pdf" download><button className="contact-btn bg-white text-purple-700 transition ease-in-out hover:scale-110" >Download Resume</button></a>
+                <a href="/resume.pdf" download><button className="contact-btn bg-white text-purple-700 transition ease-in-out hover:scale-125" >Download Resume</button></a>
             </div>
         </div>
         </>
