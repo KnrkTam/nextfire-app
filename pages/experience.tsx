8d02a0d5ee4578334e8a17b01bec61cdc051103e
kenrick tam <kenricktam@gmail.com>
2022-08-22 22:06:26 -0400

    experience font color

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -17,30 +17,30 @@ export default function Expereince() {
           {/* Board */}
           <div className='card shadow-xl lg:w-3/4'>
           <SubheadingBar title="Experience"/>            
-            <div className='bg-gray-200 rounded p-5 mt-5 grid gap-2 lg:h-96 overflow-auto animate-in fade-in duration-1000' >
+            <div className='bg-gray-200 rounded p-5 mt-5 grid gap-2  overflow-auto animate-in fade-in duration-1000' >
                 <span>
                     <a href="https://onesolution.com.hk/en/"className='font-extrabold'>One Solution Limited · Full-time</a>
                     <br></br>
                     <span className='text-sm'>Jun 2021 - Jun 2022 · 1 yr</span>
                     <ul>
-                        <li className='list-disc list-inside'> Web application development from scratch to production (CMS, ERP, E-shop)</li>
-                        <li className='list-disc list-inside'> Maintenance, Troubleshoot and enhancement for existing projects</li>
-                        <li className='list-disc list-inside'> Attend business meeting with clients for project updates and reviews</li>
+                        <li className='list-disc list-inside text-black'> Web application development from scratch to production (CMS, ERP, E-shop)</li>
+                        <li className='list-disc list-inside text-black'> Maintenance, Troubleshoot and enhancement for existing projects</li>
+                        <li className='list-disc list-inside text-black'> Attend business meeting with clients for project updates and reviews</li>
                     </ul>
-                    Skills: PHP · MySQL · CodeIgniter · Laravel
+                    Skills: PHP · MySQL · CodeIgniter · Laravel (Eloquent ORM)
                 </span>  
             </div>
-            <div className='bg-gray-200 rounded p-5 mt-5 grid gap-2 lg:h-96 overflow-auto animate-in fade-in duration-1000' >
+            <div className='bg-gray-200 rounded p-5 mt-5 grid gap-2  overflow-auto animate-in fade-in duration-1000' >
                 <span>
                     <a href="https://onesolution.com.hk/en/"className='font-extrabold'>JDT Consulting Limited · Full-time</a>
                     <br></br>
                     <span className='text-sm'>Dec 2020 - Jun 2021 · 7 months</span>
                     <ul>
-                        <li className='list-disc list-inside'> Web application development from scratch to production (CMS, ERP, E-shop)</li>
-                        <li className='list-disc list-inside'> Maintenance, Troubleshoot and enhancement for existing projects</li>
-                        <li className='list-disc list-inside'> Attend business meeting with clients for project updates and reviews</li>
+                        <li className='list-disc list-inside text-black'> Web application development from scratch to production (CMS, ERP, E-shop)</li>
+                        <li className='list-disc list-inside text-black'> Maintenance, Troubleshoot and enhancement for existing projects</li>
+                        <li className='list-disc list-inside text-black'> Attend business meeting with clients for project updates and reviews</li>
                     </ul>
-                    Skills: PHP · MySQL · CodeIgniter · Laravel
+                    Skills: JavaScript · HTML5 · CSS · MySQL
                 </span>  
             </div>
      
